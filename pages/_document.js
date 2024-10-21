08cdff7986c276038d8e5ebb4953dc46ed3dbcbe
Hamza Sheraz <hamzasheraz429@gmail.com>
2024-10-21 19:23:43 +0500

    Document page optimized

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,3 @@
-import Footer from "@/components/layout/footer";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -6,13 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className="antialiased">
-      {/* <div className="flex flex-col min-h-screen">
-      <main className="flex-grow"> */}
         <Main />
         <NextScript />
-        {/* </main> */}
-        {/* <Footer/> */}
-        {/* </div> */}
       </body>
     </Html>
   );
